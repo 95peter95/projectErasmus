180d79bbe2a93aef4102228a79b855733a47bf3e
unknown <peterpauco1@gmail.com>
2019-12-16 21:59:53 +0100

    edit image sizing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
             <div class="card mb-4 shadow-sm">
                 <div class="card-body">
                     <h5 class="card-title">{{$mobility->name}}</h5>
-                    <img src="{{$mobility->imagelink}}" height="150" >
+                    <img src="{{$mobility->imagelink}}" height="150" width="340" >
                     <p class="card-text">{{"Start date: " . $mobility->start_date}}</p>
                     <p class="card-text">{{"End date: " . $mobility->end_date}}</p>
                     <td><a class="btn btn-warning" href="{{action("MobilityController@showAction",['id' => $mobility->id])}}">Detail</a></td>
@@ -17,7 +17,7 @@
         </div>
 
 @endforeach
-    
+
 @endsection
 
 
